d63cf28f709d6dd7be57515cd068066a0fc9eac1
Hoàng Trọng Bình <62093518+hoangtrongbinh1111@users.noreply.github.com>
2020-11-27 20:42:09 +0700

    Binh_Create_Form_Main

--- Quan_Ly_Khach_San/Quan_Ly_Khach_San/Program.cs
+++ Quan_Ly_Khach_San/Quan_Ly_Khach_San/Program.cs
@@ -16,7 +16,7 @@ namespace Quan_Ly_Khach_San
           {
                Application.EnableVisualStyles();
                Application.SetCompatibleTextRenderingDefault(false);
-               Application.Run(new Form1());
+               Application.Run(new frm_Start());
           }
      }
 }
