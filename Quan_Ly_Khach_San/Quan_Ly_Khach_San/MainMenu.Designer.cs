301ed23ce3763e3e18b698f1250188920dce2728
Hoàng Trọng Bình <62093518+hoangtrongbinh1111@users.noreply.github.com>
2020-12-01 21:39:06 +0700

    Update_Form_Main_Menu_new_Binh
    cập nhật form mới

--- Quan_Ly_Khach_San/Quan_Ly_Khach_San/MainMenu.Designer.cs
+++ Quan_Ly_Khach_San/Quan_Ly_Khach_San/MainMenu.Designer.cs
@@ -37,13 +37,14 @@
                this.butQLDichVu = new System.Windows.Forms.Button();
                this.butQLDoDung = new System.Windows.Forms.Button();
                this.label1 = new System.Windows.Forms.Label();
+               this.btn_Log_out = new System.Windows.Forms.Button();
                this.SuspendLayout();
                // 
                // butDoDung
                // 
                this.butDoDung.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                this.butDoDung.Image = global::Quan_Ly_Khach_San.Properties.Resources.furniture;
-               this.butDoDung.Location = new System.Drawing.Point(600, 423);
+               this.butDoDung.Location = new System.Drawing.Point(633, 423);
                this.butDoDung.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
                this.butDoDung.Name = "butDoDung";
                this.butDoDung.Size = new System.Drawing.Size(128, 138);
@@ -52,12 +53,13 @@
                this.butDoDung.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.butDoDung.UseVisualStyleBackColor = true;
                this.butDoDung.Visible = false;
+               this.butDoDung.Click += new System.EventHandler(this.butDoDung_Click);
                // 
                // butKhachHang
                // 
                this.butKhachHang.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                this.butKhachHang.Image = global::Quan_Ly_Khach_San.Properties.Resources.Customers;
-               this.butKhachHang.Location = new System.Drawing.Point(571, 195);
+               this.butKhachHang.Location = new System.Drawing.Point(416, 423);
                this.butKhachHang.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
                this.butKhachHang.Name = "butKhachHang";
                this.butKhachHang.Size = new System.Drawing.Size(128, 138);
@@ -66,12 +68,13 @@
                this.butKhachHang.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.butKhachHang.UseVisualStyleBackColor = true;
                this.butKhachHang.Visible = false;
+               this.butKhachHang.Click += new System.EventHandler(this.butKhachHang_Click);
                // 
                // butPhong
                // 
                this.butPhong.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                this.butPhong.Image = global::Quan_Ly_Khach_San.Properties.Resources.Room;
-               this.butPhong.Location = new System.Drawing.Point(782, 195);
+               this.butPhong.Location = new System.Drawing.Point(787, 160);
                this.butPhong.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
                this.butPhong.Name = "butPhong";
                this.butPhong.Size = new System.Drawing.Size(128, 138);
@@ -80,12 +83,14 @@
                this.butPhong.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.butPhong.UseVisualStyleBackColor = true;
                this.butPhong.Visible = false;
+               this.butPhong.Click += new System.EventHandler(this.butPhong_Click);
                // 
                // butDichVu
                // 
+               this.butDichVu.BackgroundImage = global::Quan_Ly_Khach_San.Properties.Resources.btnThongTin_Glyph;
+               this.butDichVu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
                this.butDichVu.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-               this.butDichVu.Image = global::Quan_Ly_Khach_San.Properties.Resources.Service;
-               this.butDichVu.Location = new System.Drawing.Point(762, 423);
+               this.butDichVu.Location = new System.Drawing.Point(787, 324);
                this.butDichVu.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
                this.butDichVu.Name = "butDichVu";
                this.butDichVu.Size = new System.Drawing.Size(128, 138);
@@ -94,12 +99,13 @@
                this.butDichVu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.butDichVu.UseVisualStyleBackColor = true;
                this.butDichVu.Visible = false;
+               this.butDichVu.Click += new System.EventHandler(this.butDichVu_Click);
                // 
                // button1
                // 
                this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                this.button1.Image = global::Quan_Ly_Khach_San.Properties.Resources.guide;
-               this.button1.Location = new System.Drawing.Point(437, 373);
+               this.button1.Location = new System.Drawing.Point(416, 229);
                this.button1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
                this.button1.Name = "button1";
                this.button1.Size = new System.Drawing.Size(128, 138);
@@ -107,12 +113,13 @@
                this.button1.Text = "Hướng Dẫn";
                this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.button1.UseVisualStyleBackColor = true;
+               this.button1.Click += new System.EventHandler(this.button1_Click);
                // 
                // butQLPhong
                // 
                this.butQLPhong.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                this.butQLPhong.Image = global::Quan_Ly_Khach_San.Properties.Resources.room_manager;
-               this.butQLPhong.Location = new System.Drawing.Point(32, 344);
+               this.butQLPhong.Location = new System.Drawing.Point(11, 324);
                this.butQLPhong.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
                this.butQLPhong.Name = "butQLPhong";
                this.butQLPhong.Size = new System.Drawing.Size(128, 138);
@@ -121,6 +128,7 @@
                this.butQLPhong.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.butQLPhong.UseVisualStyleBackColor = true;
                this.butQLPhong.Visible = false;
+               this.butQLPhong.Click += new System.EventHandler(this.butQLPhong_Click);
                // 
                // butQLDichVu
                // 
@@ -135,12 +143,14 @@
                this.butQLDichVu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.butQLDichVu.UseVisualStyleBackColor = true;
                this.butQLDichVu.Visible = false;
+               this.butQLDichVu.Click += new System.EventHandler(this.butQLDichVu_Click);
                // 
                // butQLDoDung
                // 
+               this.butQLDoDung.BackgroundImage = global::Quan_Ly_Khach_San.Properties.Resources.buttonItemPaneCayThuMuc_Image;
+               this.butQLDoDung.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
                this.butQLDoDung.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-               this.butQLDoDung.Image = global::Quan_Ly_Khach_San.Properties.Resources.Furniture_manage;
-               this.butQLDoDung.Location = new System.Drawing.Point(277, 423);
+               this.butQLDoDung.Location = new System.Drawing.Point(182, 423);
                this.butQLDoDung.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
                this.butQLDoDung.Name = "butQLDoDung";
                this.butQLDoDung.Size = new System.Drawing.Size(128, 138);
@@ -149,23 +159,41 @@
                this.butQLDoDung.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
                this.butQLDoDung.UseVisualStyleBackColor = true;
                this.butQLDoDung.Visible = false;
+               this.butQLDoDung.Click += new System.EventHandler(this.butQLDoDung_Click);
                // 
                // label1
                // 
                this.label1.AutoSize = true;
+               this.label1.BackColor = System.Drawing.Color.Transparent;
                this.label1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-               this.label1.ForeColor = System.Drawing.Color.Fuchsia;
-               this.label1.Location = new System.Drawing.Point(185, 9);
+               this.label1.ForeColor = System.Drawing.Color.Red;
+               this.label1.Location = new System.Drawing.Point(218, 71);
                this.label1.Name = "label1";
                this.label1.Size = new System.Drawing.Size(543, 73);
                this.label1.TabIndex = 14;
                this.label1.Text = "Quản lý khách sạn";
                // 
+               // btn_Log_out
+               // 
+               this.btn_Log_out.BackColor = System.Drawing.Color.Transparent;
+               this.btn_Log_out.BackgroundImage = global::Quan_Ly_Khach_San.Properties.Resources.buttonItemMenuThoat_Image;
+               this.btn_Log_out.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+               this.btn_Log_out.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.btn_Log_out.Location = new System.Drawing.Point(840, 12);
+               this.btn_Log_out.Name = "btn_Log_out";
+               this.btn_Log_out.Size = new System.Drawing.Size(75, 60);
+               this.btn_Log_out.TabIndex = 15;
+               this.btn_Log_out.UseVisualStyleBackColor = false;
+               this.btn_Log_out.Click += new System.EventHandler(this.btn_Log_out_Click);
+               // 
                // MainMenu
                // 
                this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.BackgroundImage = global::Quan_Ly_Khach_San.Properties.Resources.furniture;
+               this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
                this.ClientSize = new System.Drawing.Size(946, 574);
+               this.Controls.Add(this.btn_Log_out);
                this.Controls.Add(this.label1);
                this.Controls.Add(this.butDoDung);
                this.Controls.Add(this.butKhachHang);
@@ -175,6 +203,7 @@
                this.Controls.Add(this.butQLPhong);
                this.Controls.Add(this.butQLDichVu);
                this.Controls.Add(this.butQLDoDung);
+               this.DoubleBuffered = true;
                this.Name = "MainMenu";
                this.Text = "MainMenu";
                this.ResumeLayout(false);
@@ -193,5 +222,6 @@
           private System.Windows.Forms.Button butQLDichVu;
           private System.Windows.Forms.Button butQLDoDung;
           private System.Windows.Forms.Label label1;
+          private System.Windows.Forms.Button btn_Log_out;
      }
 }
--- Quan_Ly_Khach_San/Quan_Ly_Khach_San/MainMenu.cs
+++ Quan_Ly_Khach_San/Quan_Ly_Khach_San/MainMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Quan_Ly_HS_GV_THPT;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,53 @@ namespace Quan_Ly_Khach_San
           public MainMenu()
           {
                InitializeComponent();
+          }
+
+          private void btn_Log_out_Click(object sender, EventArgs e)
+          {
+               Login lg = new Login();
+               lg.Show();
+               this.Hide();
+          }
+
+          private void butQLDichVu_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void butQLPhong_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void butQLDoDung_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void butKhachHang_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void butDoDung_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void butDichVu_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void butPhong_Click(object sender, EventArgs e)
+          {
+
+          }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+
           }
      }
 }
